e9553628d17700ad814eb1b27f84497a619abfbe
Niyaz Tukhbatullin <niyaztukhbatullin2004@gmail.com>
2025-05-30 21:03:01 +0300

    working with reviews and authentication

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -103,5 +103,41 @@ export async function createOffer(req, res, next) {
     }
 }
 
+async function getFavoriteOffers(req, res, next) {
+    try {
+        // Находим все предложения, где isFavorite = true
+        const favoriteOffers = await Offer.findAll({
+            where: {
+                isFavorite: true
+            }
+        });
+        
+        // Адаптируем данные для клиента
+        const adaptedOffers = favoriteOffers.map(adaptOfferToClient);
+        
+        res.status(200).json(adaptedOffers);
+    } catch (error) {
+        next(ApiError.internal('Не удалось получить избранные предложения'));
+    }
+}
 
-export { getAllOffers, getFullOffer };
+const toggleFavorite = async (req, res, next) => {
+    try {
+        const { offerId, status } = req.params;
+
+        const offer = await Offer.findByPk(offerId);
+        if (!offer) {
+            return next(ApiError.notFound('Предложение не найдено'))
+        }
+
+        offer.isFavorite = status === '1';
+        await offer.save();
+
+        res.json(offer);
+    } catch (error) {
+        next(ApiError.internal('Ошибка при обновлении статуса избранного'));
+    }
+};
+
+
+export { getAllOffers, getFullOffer, getFavoriteOffers, toggleFavorite };
--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createOffer, getAllOffers, getFullOffer } from "../controllers/offerController.js";
+import { createOffer, getAllOffers, getFullOffer, getFavoriteOffers, toggleFavorite } from "../controllers/offerController.js";
 import upload from "../middleware/upload.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = new Router();
 
@@ -9,6 +10,8 @@ router.post('/offers', upload.fields([
     { name: 'previewImage', maxCount: 1 },
     { name: 'photos', maxCount: 6 }
 ]), createOffer);
-router.get('/offers/:id', getFullOffer)
+router.get('/offers/:id', getFullOffer);
+router.get('/favorite', getFavoriteOffers);
+router.post('/favorite/:offerId/:status', authenticateToken, toggleFavorite);
 
 export default router;
